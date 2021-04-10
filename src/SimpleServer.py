c09316dd4d07f4aa0c99bfc566d617ce2f94dde1
Taylor McCampbell <tmccampb@uwyo.edu>
2021-04-10 17:09:22 +0000

    Cleaning up code base

--- src/SimpleServer.py
+++ src/SimpleServer.py
@@ -74,32 +74,6 @@ def surnameSearch():
             conn.close()
             return render_template('Employee.html', data=data)
 
-
-# The name says it...
-# @app.route("/Employee/VulnerableSearch", methods=['GET', 'POST'])
-# def surnameInjectionSearch():
-#     if request.method == 'GET':
-#         return render_template('EmployeeSQLInjection.html')
-#     if request.method == 'POST':
-#         # rem: args for get form for post
-#         lastName = request.form.get('lastName', default="Error")
-#         conn = sqlite3.connect(DATABASE)
-#         cur = conn.cursor()
-
-#         # VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
-#         query = "SELECT * FROM EmployeeList WHERE lastname= '%s' " % (
-#             lastName,)
-#         print(query)
-#         cur.execute(query)
-#         # VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD VERY BAD
-
-#         data = cur.fetchall()
-#         print(data)
-#         print(lastName)
-#         conn.close()
-#         return render_template('Employee.html', data=data)
-
-
 @app.route("/Employee/Modify", methods=['GET', 'POST'])
 def modifyEmployee():
     if request.method == 'GET':
@@ -122,8 +96,6 @@ def modifyEmployee():
             conn = sqlite3.connect(DATABASE)
             cur = conn.cursor()
             cur.execute("UPDATE EmployeeList SET [FirstName] = ?, [LastName] = ?, [FT/PT/.75?] = ?, [Business Unit] = ?, [City] = ?, [State/Province] = ?, [Career Matrix Title] = ?, [Total Years] = ?, [Registered Licenses] = ?, [Skill] = ?, [Skill Level] = ? WHERE [LastName] = ?", (firstName, lastName, ftpt, businessunit, city, state, cmt, totalYears, registeredLicenses, skill, skillLevel, lastName))   
-            # "UPDATE EmployeeList SET ('FirstName', 'LastName', 'FT/PT/.75?', 'Business Unit', 'City', 'State/Province', 'Career Matrix Title', 'Total Years', 'Registered Licenses', 'Skill', 'Skill Level') VALUES (?,?,?,?,?,?,?,?,?,?,?) WHERE ('LastName') VALUES (?)", (firstName, lastName, ftpt, businessunit, city, state, cmt, totalYears, registeredLicenses, skill, skillLevel), (lastName)) 
-            # "UPDATE EmployeeList SET 'FirstName'=%s, 'FT/PT/.75'=%s, 'Business Unit'=%s, 'City'=%s, 'State/Province'=%s, 'Career Matrix Title'=%s, 'Total Years'=%s, 'Registered Licenses'=%s, 'Skill'=%s, 'Skill Level'=%s WHERE 'LastName'=%s" %(firstName, ftpt, businessunit, city, state, cmt, totalYears, registeredLicenses, skill, skillLevel, lastName))
             conn.commit()
             if cur.rowcount < 1:
                 msg = "Update failed"
@@ -136,15 +108,6 @@ def modifyEmployee():
             conn.close()
             print(msg)
             return msg
-
-
-@app.route("/Employee/Main", methods=['GET', 'POST'])
-def mainFunctionality():
-    if request.method == 'GET':
-        return render_template('Main.html')
-    if request.method == 'POST':
-        # Queries and such here, also functionality for filters once they appear
-        return render_template('Main.html')  # Might need to rename this
 
 
 if __name__ == "__main__":
