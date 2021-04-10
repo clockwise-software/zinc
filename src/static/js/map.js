fcb6508b3ac4dcd51132d07cf3c753629ab96ba3
Cody Danielson <cody.danielson@gmail.com>
2021-04-10 15:32:11 -0600

    Add padding to map auto zooming

--- src/static/js/map.js
+++ src/static/js/map.js
@@ -1,45 +1,92 @@
 let map; // google.map.Map
+let dbReturn = []; // array of employee data
+let markers = {}; // google.map.Marker array
+let employees = []; // array of employees description and location
 let bounds; // bounds for autozooming of map
-let locations = []; // google.maps.LatLng array
-let markers = []; // google.map.Marker array
+let latLngs = [];
+
+let citiesDB = {};
 
 function initMap() {
-	map = new google.maps.Map(document.getElementById("map"), {
+	map = new google.maps.Map(document.getElementById('map'), {
 		mapTypeId: 'terrain',
 		disableDefaultUI: true,
 	});
 
-	const cheyenne = new google.maps.LatLng(41.14, -104.820278);
-	const laramie = new google.maps.LatLng(41.311111, -105.593611);
-	// const fortCollins = new google.maps.LatLng(40.559167, -105.078056);
-	const boundsPadding = 15;
+	// db simulated employee query return:
+	let employee1 = {fname: 'April', lname: 'Aguilar', Unit: 'IN GIS/CADD Services', City: 'Laramie', State: 'Wyoming', Title: 'Project GIS Analyst', Licenses: '', Skills: 'Geologist'};
+	let employee2 = {fname: 'Isaac', lname: 'Baldwin', Unit: 'ICE Technical Services', City: 'Laramie', State: 'Wyoming', Title: 'Senior Geologist/Hydrogeologist', Licenses: 'PG', Skills: ''};
+	let employee3 = {fname: 'Brendan', lname: 'Ball', Unit: 'Business Development and Marketing (BDM)', City: 'Fort Collins', State: 'Colorado', Title: 'Senior BD Specialist', Licenses: '', Skills: 'Contaminant Modeling'};
+	let employee4 = {fname: 'Wayne', lname: 'Carlson', Unit: 'PPS Mid-Continent', City: 'Cheyenne', State: 'Wyoming', Title: 'Associate Geologist', Licenses: 'P.G.', Skills: ''};
+	dbReturn = [employee1, employee2, employee3, employee4];
 
-	locations = [cheyenne, laramie];
-	bounds = findBounds(locations);
+	// db simulated query return:
+	citiesDB['Laramie, Wyoming'] = {lat: 41.311111, lng: -105.593611};
+	citiesDB['Fort Collins, Colorado'] = {lat: 40.559167, lng: -105.078056};
+	citiesDB['Cheyenne, Wyoming']  = {lat: 41.14, lng: -104.820278};
+
+	// make employees list
+	for(let i = 0; i < dbReturn.length; i++) {
+		let employee = {};
+		employee.description = getDescription(dbReturn[i]); // employee description shown on map marker
+		employee.location = getLocation(dbReturn[i]); // location city, name string
+		employee.latLng = getLatLng(dbReturn[i]);
+		employees.push(employee);
+	}
+
+	// make markers list
+	for(let i = 0; i < employees.length; i++) {
+		employee = employees[i];
+		if(employee.location in markers) {
+			// add to existing marker
+			markers[employee.location].description += '<br><br>' + employee.description;
+		} else {
+			// add new marker
+			let marker = {};
+			marker.description = employee.description;
+			marker.latLng = employee.latLng;
+			marker.InfoWindow = new google.maps.InfoWindow();
+			latLngs.push(employee.latLng);
+			markers[employee.location] = marker;
+		}
+	}
+
+	// display all markers on map
+	for(const [key, value] of Object.entries(markers)) {
+		addMarker(value);
+	}
+
+	// Autofit map
+	const boundsPadding = 35;
+	bounds = findBounds(latLngs);
 	map.fitBounds(bounds, boundsPadding);
-
-	// This event listener will call addMarker() when the map is clicked.
-	// map.addListener("click", (event) => {
-	// 	addMarker(event.latLng, "another test");
-	// });
-
-	// test of combinded markers
-	addMarker(laramie, "<b>April Aguilar</b><br>IN GIS/CADD Services<br>Project GIS Analyst<br>Registered Licenses:");
-	addMarker(cheyenne, "<b>Willie Carpenter</b><br>PPS Mid-Continent<br>Senior Geologist/Hydrogeologistt<br>Registered Licenses: PG" +
-						"<br><br>" +
-						"<b>Anthony Collier</b><br>IN Water Resources<br>Senior Engineer<br>Registered Licenses:");
 }
 
-function addMarker(location, label) {
-	const marker = new google.maps.InfoWindow();
-	marker.setContent(label);
-	marker.setPosition(location);
-	marker.open(map);
-	map.addListener("zoom_changed", () => {
-		marker.setContent(label);
-		marker.open(map);
-	});
-	markers.push(marker);
+// generates employee description to be displayed on map
+function getDescription(employee) {
+	let name = employee.fname + ' ' + employee.lname;
+	let results = '<b>' + name + '</b><br>' +
+					employee.Unit + '<br>' +
+					employee.Title + '<br>' +
+					'Licenses: ' + employee.Licenses;
+	return results;
+}
+
+// returns city, state string from employee db entry
+function getLocation(employee) {
+	return employee.City + ', ' + employee.State;
+}
+
+// return maps.google.LatLng literal
+function getLatLng(employee) {
+	let city = employee.City + ', ' + employee.State;
+	return citiesDB[city];
+}
+
+function addMarker(marker) {
+	marker.InfoWindow.setContent(marker.description);
+	marker.InfoWindow.setPosition(marker.latLng);
+	marker.InfoWindow.open(map);
 }
 
 // Sets the map on all markers in the array.
@@ -69,20 +116,22 @@ function deleteMarkers() {
 	markers = [];
 }
 
+// Takes all locations and finds the min/max of all lat/lngs to find north, south, east, and west bounds
+// returns as a google.maps.latLng literal
 function findBounds(locations) {
 
 	if(locations.length <= 0) {
 		return;
 	}
 
-	let north = locations[0].lat();
-	let south = locations[0].lat();
-	let east = locations[0].lng();
-	let west = locations[0].lng();
+	let north = locations[0].lat;
+	let south = locations[0].lat;
+	let east = locations[0].lng;
+	let west = locations[0].lng;
 
-	for(i = 0; i < locations.length; i++) {
-		let lat = locations[i].lat();
-		let lng = locations[i].lng();
+	for(let i = 0; i < locations.length; i++) {
+		let lat = locations[i].lat;
+		let lng = locations[i].lng;
 		
 		// north bounds
 		if(lat > north) {
